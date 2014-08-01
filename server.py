686b878e673cd1580fd32d119b8fb5a6511ab0ca
mupchrch <mupchrch@users.noreply.github.com>
2014-08-01 00:10:49 -0400

    edited to work on amazon ec2 and on port 80

--- server.py
+++ server.py
@@ -5,49 +5,33 @@ import os
 
 from tornado.options import define, options, parse_command_line
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=80, help="run on the given port", type=int)
 
-#list of connected clients
+# we gonna store clients in dictionary..
 clients = []
 
 class IndexHandler(tornado.web.RequestHandler):
-    @tornado.web.asynchronous
-    def get(self):
-        self.render("index.html")
+   @tornado.web.asynchronous
+   def get(self):
+      self.render("website/index.html")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    def open(self):
-        clients.append(self)
-        self.stream.set_nodelay(True)
-        self.write_message(u"Welcome to KappaFeed!")
+   def open(self):
+      clients.append(self)
+      self.stream.set_nodelay(True)
+      self.write_message(u"Welcome to KappaFeed!")
 
-    def on_message(self, message):
-        print "~SERVER~ Received a message : %s" % (message)
-        self.write_message(u"You said: " + message)
-        
-    def on_close(self):
-        clients.remove(self)
+   def on_message(self, message):
+      print "~SERVER~ Received a message : %s" % (message)
+      self.write_message(u"You said: " + message)
+
+   def on_close(self):
+      clients.remove(self)
 
 def sendToClients(message):
-    for client in clients:
-        if not client.ws_connection.stream.socket:
-            print "~SERVER~ client left"
-            clients.remove(client)
-        else:
-            client.write_message(message)
-
-settings = {
-    "static_path": os.path.join(os.path.dirname(__file__), "static"),
-}
-
-app = tornado.web.Application([
-    (r'/', IndexHandler),
-    (r'/feed/', WebSocketHandler),
-    (r"/(kappa\.js)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
-    (r"/(kappa\.css)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
-])
-
-def startServer():
-    parse_command_line()
-    app.listen(options.port)
-    tornado.ioloop.IOLoop.instance().start()
+   for client in clients:
+      if not client.ws_connection.stream.socket:
+         print "~SERVER~ client left"
+         clients.remove(client)
+      else:
+         client.write_message(message)
