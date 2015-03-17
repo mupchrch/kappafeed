0d1b0e2c8f0944591c1a686667dfd19dec44c8ae
Mike Upchurch <mjupchurch@comcast.net>
2015-03-17 05:06:33 +0000

    Fixed event server trying to connect when no events.

--- KappaFeed.py
+++ KappaFeed.py
@@ -1,6 +1,7 @@
 import Logger
 import IrcConnection
 import TwitchApi
+import server
 
 from threading import Thread
 
@@ -19,14 +20,15 @@ class KappaFeed(object):
 
         while True:
             try:
+                ircInstantiated = False;
+                eventIrcInstantiated = False;
+
                 irc = IrcConnection.IrcConnection(self.oauthToken, self.userName)
-                irc.connect(self.serverAddress, self.portNumber, True)
 
                 eventIrc = IrcConnection.IrcConnection(self.oauthToken, self.userName)
-                eventIrc.connect(self.eventServerAddress, self.portNumber, True)
 
                 twitchApi = TwitchApi.TwitchApi()
-
+                #server.sendToClients({'serverMsg': 'Refreshing top streams list. One moment.'})
                 while True:
                     topChannels = twitchApi.getTopChannels(self.numChannelsToJoin)
                     channelsJoined = []
@@ -35,29 +37,43 @@ class KappaFeed(object):
                     #detemine whether to join on event chat or regular
                     for chan in topChannels:
                         if twitchApi.getEventChatStatus(chan):
+                            if eventIrcInstantiated == False:
+                                eventIrc.connect(self.eventServerAddress, self.portNumber, True)
+                                eventIrcInstantiated = True
                             if eventIrc.joinChannel(chan):
                                 eventChannelsJoined.append(chan)
                         else:
+                            if ircInstantiated == False:
+                                irc.connect(self.serverAddress, self.portNumber, True)
+                                ircInstantiated = True
                             if irc.joinChannel(chan):
                                 channelsJoined.append(chan)
 
-                    t1 = Thread(target=irc.channelScan, args=['25'])
-                    t2 = Thread(target=eventIrc.channelScan, args=['25'])
-                    t1.start()
-                    t2.start()
+                    if ircInstantiated:
+                        t1 = Thread(target=irc.channelScan, args=['25'])
+                        t1.start()
+                    if eventIrcInstantiated:
+                        t2 = Thread(target=eventIrc.channelScan, args=['25'])
+                        t2.start()
 
-                    t1.join()
-                    t2.join()
+                    if ircInstantiated:
+                        t1.join()
+                    if eventIrcInstantiated:
+                        t2.join()
 
-                    channelsNotParted = irc.partChannels(channelsJoined)
-                    if len(channelsNotParted) > 0:
-                        break
+                    if ircInstantiated:
+                        channelsNotParted = irc.partChannels(channelsJoined)
+                        if len(channelsNotParted) > 0:
+                            break
 
-                    eventChannelsNotParted = eventIrc.partChannels(eventChannelsJoined)
-                    if len(eventChannelsNotParted) > 0:
-                        break
+                    if eventIrcInstantiated:
+                        eventChannelsNotParted = eventIrc.partChannels(eventChannelsJoined)
+                        if len(eventChannelsNotParted) > 0:
+                            break
 
                     self.kfLogger.log('Refreshing top streams...')
                 self.kfLogger.log('Restarting...')
+                #server.sendToClients({'serverMsg': 'Refreshing top streams list. One moment.'})
             except Exception, e:
                 self.kfLogger.log('Error, restarting... %s' % str(e))
+                #server.sendToClients({'serverMsg': 'Refreshing top streams list. One moment.'})
